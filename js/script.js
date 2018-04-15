9a109bb4d5020232cee13c9a11c5ba5da67d8bef
Darthseid <jared.mcvey@yahoo.com>
2018-04-15 16:40:44 -0500

    Added automatic slideshow function.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-(function( )) {
-	
+var slidenumber = 0;
+showSlideshow();
+
+function showSlideshow() 
+{
+    var i;
+    var slide = document.getElementsByClassName("Slideshow_fade");
+    for (i = 0; i < slide.length; i++) 
+	{
+        slides[i].style.display = "none"; 
+    }
+    slidenumber++;
+    if (slidenumber > slide.length) {slidenumber = 1} 
+    slide[slidenumber-1].style.display = "block"; 
+    setTimeout(showSlides, 3000); // Change image after three seconds
 }
