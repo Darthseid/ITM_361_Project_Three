de242278bfa70c49fcf471e26234b57bc6770f19
Darthseid <jared.mcvey@yahoo.com>
2018-04-15 16:45:48 -0500

    Added manual slideshow

--- js/script.js
+++ js/script.js
@@ -1,16 +1,29 @@
-var slidenumber = 0;
-showSlideshow();
+var slidenumber = 1;
+showSlideshow(slidenumber);
 
-function showSlideshow() 
-{
-    var i;
-    var slide = document.getElementsByClassName("Slideshow_fade");
-    for (i = 0; i < slide.length; i++) 
-	{
-        slides[i].style.display = "none"; 
-    }
-    slidenumber++;
-    if (slidenumber > slide.length) {slidenumber = 1} 
-    slide[slidenumber-1].style.display = "block"; 
-    setTimeout(showSlides, 3000); // Change image after three seconds
+// Next/previous control panel
+function plusSlideshow(i) {
+  showSlides(slideIndex += i);
+}
+
+// image control panel
+function presentSlide(i) {
+  showSlideshow(slidenumber = i);
+}
+
+function showSlides(i) {
+  var n;
+  var slide = document.getElementsByClassName("Slideshow_fade");
+  var dot = document.getElementsByClassName("circle");
+  if (i > slide.length) {slidenumber = 1} 
+  if (i < 1) {slidenumber = slide.length}
+  for (n = 0; n < slide.length; n++) {
+      slide[n].style.display = "none"; 
+  }
+  for (n = 0; n < dot.length; n++) 
+  {
+      dot[n].className = dot[n].className.replace(" activated", "");
+  }
+  slide[slidenumber-1].style.display = "blocked"; 
+  dots[slidenumber-1].className += " activated";
 }
