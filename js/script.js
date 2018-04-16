03f235c8b84a842addf9efcf555e331c987c29d0
Darthseid <jared.mcvey@yahoo.com>
2018-04-15 23:27:27 -0500

    Changed variable names.

--- js/script.js
+++ js/script.js
@@ -1,29 +1,22 @@
-var slidenumber = 1;
-showSlideshow(slidenumber);
-
-// Next/previous control panel
-function plusSlideshow(i) {
-  showSlides(slideIndex += i);
+var slide_number = 0; 
+ function slidechange()
+{  
+       	var i;
+    var slides = document.getElementsByClassName("sliding");
+    var circle = document.getElementsByClassName("circles");
+    for (i = 0; i < slides.length; i++) 
+    {
+       slides[i].style.display = "none";  
+    }
+    slide_number++;
+    if (slide_number > slides.length) {slide_number = 1}    
+    for (i = 0; i < circle.length; i++) 
+    {
+        circle[i].className = circle[i].className.replace(" activated", "");
+    }
+    slides[slide_number-1].style.display = "block";  
+    circle[slide_number-1].className += " activated";
+    setTimeout(slidechange, 2500); // Change image every 2 and a half seconds
 }
+	
 
-// image control panel
-function presentSlide(i) {
-  showSlideshow(slidenumber = i);
-}
-
-function showSlides(i) {
-  var n;
-  var slide = document.getElementsByClassName("Slideshow_fade");
-  var dot = document.getElementsByClassName("circle");
-  if (i > slide.length) {slidenumber = 1} 
-  if (i < 1) {slidenumber = slide.length}
-  for (n = 0; n < slide.length; n++) {
-      slide[n].style.display = "none"; 
-  }
-  for (n = 0; n < dot.length; n++) 
-  {
-      dot[n].className = dot[n].className.replace(" activated", "");
-  }
-  slide[slidenumber-1].style.display = "blocked"; 
-  dots[slidenumber-1].className += " activated";
-}
